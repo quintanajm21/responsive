4544e349f4ac68195e875dfe14abc58071ab67fe
Jose Manuel <josemanuelmartinezquintana@gmail.com>
2020-11-04 08:53:29 +0100

    Responsive, lista de contactos

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
+    HttpClientModule,
     HomePageRoutingModule
   ],
   declarations: [HomePage]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  users: any;
+
+  constructor(private httpClient:  HttpClient) {
+    this.users = this.httpClient.get('https://randomuser.me/api/?results=20').pipe(map(res => res['results']));
+  }
 
 }
